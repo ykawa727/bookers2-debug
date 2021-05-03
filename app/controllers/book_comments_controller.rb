3c762af3c5bbf3470bd0fd996a63cb88eae0521a
ykawa727 <y.aikawa727@gmail.com>
2021-05-03 12:13:31 +0000

    [Learn3] ouyou3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = BookComment.find(params[:book_id])
+    book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
     redirect_back(fallback_location: root_path)
-  end 
-  
+  end
+
   def destroy
-  end 
-  
+  end
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
-  end 
+  end
 end
