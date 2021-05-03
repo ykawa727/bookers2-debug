b93e79b4a4c3b902676a1a13e4b3792ab8c8748f
ykawa727 <y.aikawa727@gmail.com>
2021-05-03 11:56:59 +0000

    [Learn2] ouyou3

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,7 @@ class Book < ApplicationRecord
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
 	end
+	has_many :book_comments, dependent: :destroy
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy 
+  has_many :book_comments, dependent: :destroy
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
