9a450db84145b34cb7d7c4dafbca9a513b4254f5
ykawa727 <y.aikawa727@gmail.com>
2021-05-03 04:53:02 +0000

    [Start] ouyou3

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'homes#top'
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   get 'home/about' => 'homes#about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
