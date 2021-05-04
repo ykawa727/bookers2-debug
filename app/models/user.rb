fa16acd11baf0f36126d145d9ca4feccfc389440
ykawa727 <y.aikawa727@gmail.com>
2021-05-04 09:43:11 +0000

    [Start] ouyou4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,14 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy 
-  has_many :book_comments, dependent: :destroy
+  has_many :book_comments, dependent: :destroy 
+  
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
